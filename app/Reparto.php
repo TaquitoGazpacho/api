687a0f65f2c4139c5dc699b03a32767e259287ee
gondhalf <gondhalf@git.com>
2018-01-24 09:38:10 +0100

    Repartos controller

---
+++ app/Reparto.php
@@ -0,0 +1,41 @@
+<?php
+
+namespace App;
+
+use Illuminate\Database\Eloquent\Model;
+
+class Reparto extends Model
+{
+    protected $fillable = [
+        'clave_repartidor', 'clave_usuario', 'usuario_id','empresa_id', 'oficina_id', 'taquilla_id',
+    ];
+
+    public static function getRepartos() {
+        $empresa_id = Auth::guard('empresa')->user()->id;
+
+        $repartos = Reparto::where('empresa_id', $empresa_id)->get();
+
+
+        return $repartos;
+    }
+
+    public function usuario()
+    {
+        return $this->belongsTo('App\Models\User');
+    }
+
+    public function empresa()
+    {
+        return $this->belongsTo('App\Models\Empresa_reparto');
+    }
+
+    public function oficina()
+    {
+        return $this->belongsTo('App\Models\Oficina');
+    }
+
+    public function taquilla()
+    {
+        return $this->belongsTo('App\Models\Taquilla');
+    }
+}
