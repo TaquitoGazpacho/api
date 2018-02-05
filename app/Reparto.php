8913b46ad8507b5aec63236165e55eb19c964f51
Andretxu6 <aespadamu16dw@ikzubirimanteo.com>
2018-02-05 09:14:03 +0100

    mail pedido depositado

--- app/Reparto.php
+++ app/Reparto.php
@@ -1,41 +1,57 @@
 <?php
+namespace App\Models;
 
-namespace App;
+use Illuminate\Notifications\Notifiable;
+use Illuminate\Foundation\Auth\User as Authenticatable;
+use Illuminate\Contracts\Auth\CanResetPassword;
 
-use Illuminate\Database\Eloquent\Model;
-
-class Reparto extends Model
+class User extends Authenticatable
 {
+    use Notifiable;
+
+    /**
+     * The attributes that are mass assignable.
+     *
+     * @var array
+     */
     protected $fillable = [
-        'clave_repartidor', 'clave_usuario', 'usuario_id','empresa_id', 'oficina_id', 'taquilla_id',
+        'name', 'surname', 'phone', 'sex', 'email', 'password', 'image', 'email_token', 'suscripcion_id', 'oficina_id', 'verified',
     ];
 
-    public static function getRepartos() {
-        $empresa_id = Auth::guard('empresa')->user()->id;
+    /**
+     * The attributes that should be hidden for arrays.
+     *
+     * @var array
+     */
+    protected $hidden = [
+        'password', 'remember_token',
+    ];
 
-        $repartos = Reparto::where('empresa_id', $empresa_id)->get();
-
-
-        return $repartos;
-    }
-
-    public function usuario()
+    public function reparto()
     {
-        return $this->belongsTo('App\Models\User');
+        return $this->hasMany('App\Models\Reparto');
     }
 
-    public function empresa()
+    public function suscripcion()
     {
-        return $this->belongsTo('App\Models\Empresa_reparto');
+        return $this->belongsTo('App\Models\Suscripcion');
     }
-
     public function oficina()
     {
         return $this->belongsTo('App\Models\Oficina');
     }
-
-    public function taquilla()
-    {
-        return $this->belongsTo('App\Models\Taquilla');
+    public function changeImage($image){
+        User::where('id', $this->id)
+            ->update(['image' => 'img/userImg/'.$image]);
+    }
+    public function cambiarOficina($office_id){
+        User::where('id', $this->id)
+            ->update(['oficina_id'=>$office_id]);
+    }
+    public function isVerified(){
+        return $this->verified;
+    }
+    public static function getUsuarios(){
+        return User::get();
     }
 }
