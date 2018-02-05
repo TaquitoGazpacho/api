db380180c86d127ba1db14977d70fd755e49b21b
Andretxu6 <aespadamu16dw@ikzubirimanteo.com>
2018-02-05 09:29:26 +0100

    fallos varios

--- resources/views/mail/pedidoDepositado.blade.php
+++ resources/views/mail/pedidoDepositado.blade.php
@@ -1,7 +1,7 @@
 
 <body style="border-radius:10px; max-width: 500px; text-align: center; border: 1px solid #92BCE0; margin:auto">
 <h1 style="background: #3C8DBC; color: white; border-top: 2px solid #3071A9; border-bottom: 2px solid #3071A9">Lock<span style="color: #F96F00">Box</span></h1>
-<h3 style="color: black">Hola, {{ $name }}, tu pedido se ha depositado correctamente</h3>
+<h3 style="color: black">Hola, {{ $nombre }}, tu pedido se ha depositado correctamente</h3>
 <p style="color: black">Puedes pasar a recogerlo a cualquier hora en tu oficina seleccionada: {{ $oficina }}</p>
 <p style="color: black">Podrás encontrar tu pedido en la taquilla {{ $taquilla }}. Tu clave es {{ $clave }}</p>
 <a style="color: white;line-height: 50px;width: 300px; height: 50px; background: #F96F00; border: 1px solid #bc5600; text-decoration: none;display: inline-block;" href="{{ route('home.pedidos') }}">Tus pedidos</a>
