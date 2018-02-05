34c2f640f835fff51a91622617e0459af645fd51
Andretxu6 <aespadamu16dw@ikzubirimanteo.com>
2018-02-05 09:30:29 +0100

    fallos varios

--- resources/views/mail/pedidoDepositado.blade.php
+++ resources/views/mail/pedidoDepositado.blade.php
@@ -4,6 +4,6 @@
 <h3 style="color: black">Hola, {{ $nombre }}, tu pedido se ha depositado correctamente</h3>
 <p style="color: black">Puedes pasar a recogerlo a cualquier hora en tu oficina seleccionada: {{ $oficina }}</p>
 <p style="color: black">Podrás encontrar tu pedido en la taquilla {{ $taquilla }}. Tu clave es {{ $clave }}</p>
-<a style="color: white;line-height: 50px;width: 300px; height: 50px; background: #F96F00; border: 1px solid #bc5600; text-decoration: none;display: inline-block;" href="{{ route('home.pedidos') }}">Tus pedidos</a>
+<a style="color: white;line-height: 50px;width: 300px; height: 50px; background: #F96F00; border: 1px solid #bc5600; text-decoration: none;display: inline-block;" href="http://electricsheep.es/taquitoGazpacho/perfil/pedidos">Tus pedidos</a>
 </body>
 <br/>
